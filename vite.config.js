3cec0d14200c930bbdf8b8ed640213bcc0176797
Barathraj <barathdeva757@gmail.com>
2023-11-27 21:55:19 +0530

    vite and bootstrap setup

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import path from 'path';
+
+export default {
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+};
